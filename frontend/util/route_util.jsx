41e8f64145cb4baa78fcf3522c828fc163362c56
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-05-21 08:17:05 -0700

    Fixed an issue with Auth and Protected routes

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -3,32 +3,24 @@ import { connect } from "react-redux";
 import { Route, Redirect, withRouter} from "react-router-dom";
 
 const Auth = ({ component: Component, path, loggedIn, exact }) => {
-  return <Route path={path} exact={exact} render={props => {
-    return (
-      !loggedIn ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to="/" />
-      )
-    );
-  }}/>
-};
+  return <Route 
+    path={path} 
+    exact={exact} 
+    render={props => !loggedIn ?  <Component {...props} /> : <Redirect to="/" /> } 
+  />
+}; 
 
 const Protected = ({ component: Component, path, loggedIn, exact }) => {
-  return <Route path={path} exact={exact} render={props => {
-    return (
-      !loggedIn ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to="/login" />
-      )
-    );
-  }}/>
+  return <Route 
+    path={path} 
+    exact={exact} 
+    render={props => !loggedIn ?  <Component {...props} /> : <Redirect to="/login" /> } 
+  />
 };
 
 const mstp = state => {
   return {
-    loggedIn: Boolean(state.session.id)
+    loggedIn: Boolean(state.session.currentUserId)
   };
 };
 
