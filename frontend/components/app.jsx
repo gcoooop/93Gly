abe382613435b256e8874ad7b5cfc9f2f2ba1dcc
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-05-22 14:16:49 -0700

    Commented out a nonexistent import file to solve heroku deploy error

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
-import Splash from "./splash/splash";
+// import Splash from "./splash/splash";
 import NavBar from "./navbar/navbar";
 import SignupFormContainer from "./session/signup_form_container";
 import LoginFormContainer from "./session/login_form_container";
