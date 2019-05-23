0c1a9f44575ac434972898532881eee4a3b59680
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-05-23 13:02:40 -0700

    Added a barebones footer and began styling the splash page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -6,7 +6,6 @@ const Splash = props => {
   return (
     <div className="splash">
       <div className="splash-slider">
-        <button>{"<"}</button>
 
         <div className="splash-slider-image">
           <img/>
@@ -16,11 +15,10 @@ const Splash = props => {
         <div className="splash-slider-caption">
           <h3>The universal network for astrophotographers</h3>
           <h1>Discover and share the universe's best photos</h1>
-          <p>Get inspired with incredible photos of celestial bodies across the universe. We're not guided by fads - just great astrophotography.</p>
-          <Link to="/signup">Sign up</Link>
+          <p>Get inspired with incredible photos of celestial bodies across the universe. We're not guided by fads - just the stars.</p>
+          <Link to="/signup"><span>Sign up</span></Link>
         </div>
 
-        <button>{">"}</button>
       </div>
     </div>
   );
