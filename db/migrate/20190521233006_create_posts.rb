a950450c6e42366d9e0086899e7c016451ade4c1
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-05-27 20:10:58 -0700

    Changed database constraints for posts' titles and captions to be present and default to an empty string

--- db/migrate/20190521233006_create_posts.rb
+++ db/migrate/20190521233006_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :title, null: false
-      t.string :caption
+      t.string :title, null: false, default: ""
+      t.string :caption, null: false, default: ""
       t.integer :photographer_id, null: false
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_21_233006) do
+ActiveRecord::Schema.define(version: 2019_05_28_025205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,17 +36,8 @@ ActiveRecord::Schema.define(version: 2019_05_21_233006) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "photos", force: :cascade do |t|
-    t.string "title", null: false
-    t.string "caption"
-    t.integer "photographer_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["photographer_id"], name: "index_photos_on_photographer_id"
-  end
-
   create_table "posts", force: :cascade do |t|
-    t.string "title", null: false
+    t.string "title"
     t.string "caption"
     t.integer "photographer_id"
     t.datetime "created_at", null: false
