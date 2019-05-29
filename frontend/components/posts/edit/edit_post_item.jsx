56d9a3e2e9f2c1eba5116a90da3ba1e03540ba5f
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-05-28 21:14:44 -0700

    Added a masonry grid style to the post index

--- frontend/components/posts/edit/edit_post_item.jsx
+++ frontend/components/posts/edit/edit_post_item.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const EditPostItem = (props) => {
   return (
-    <div className={`post-grid-item ${props.selected}`} onClick={props.updateSelection} style={ {backgroundImage: props.post.photoUrl} }>
+    <div className={`post-grid-item ${props.selected}`} onClick={props.updateSelection} style={ {backgroundImage: `url(${props.post.photoUrl})`} }>
       {/* <img src={props.post.photoUrl} onClick={props.updateSelection}/> */}
       <div className="item-top item-info"></div>
       <div className="item-bottom item-info"></div>
--- frontend/components/posts/post_index_item.jsx
+++ frontend/components/posts/post_index_item.jsx
@@ -2,8 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const PostIndexItem = props => {
+  const img = new Image();
+  img.src = props.post.photoUrl;
+  const imgW = img.width; 
+  const imgH = img.height; 
+  const calcW = 300 * imgW / imgH;
+  const styles = {
+    backgroundImage: `url(${props.post.photoUrl})`,
+    flexGrow: 1,
+    flexShrink: 0,
+    flexBasis: `${calcW}px`
+  };
   return (
-    <div className="post-grid-item" style={ {backgroundImage: `url(${props.post.photoUrl})`} }>
+    <div className="post-grid-item" style={ styles }>
       <Link to={`/posts/${props.post.id}`}>
         {/* <img src={props.post.photoUrl}/> */}
       </Link>
