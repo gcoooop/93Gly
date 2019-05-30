e29dfb7784943a99b25e035cc99776d0ba9df058
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-05-30 08:46:42 -0700

    Images now load with proper dimensions on their first render

--- frontend/components/posts/post_index_item.jsx
+++ frontend/components/posts/post_index_item.jsx
@@ -1,25 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const PostIndexItem = props => {
-  const img = new Image();
-  img.src = props.post.photoUrl;
-  const imgW = img.width; 
-  const imgH = img.height; 
-  const calcW = 300 * imgW / imgH;
-  const styles = {
-    backgroundImage: `url(${props.post.photoUrl})`,
-    flexGrow: 1,
-    flexShrink: 0,
-    flexBasis: `${calcW}px`,
-  };
-  return (
-    <div className="post-grid-item" style={ styles }>
-      <Link to={`/posts/${props.post.id}`}></Link>
-      <div className="item-top item-info"></div>
-      <div className="item-bottom item-info"></div>
-    </div>
-  );
-};
+class PostIndexItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loaded: false };
+  }
+
+  render()  {
+    const img = new Image();
+    img.src = this.props.post.photoUrl;
+    img.onloadend = () => this.setState({ loaded: true });
+    const imgW = img.width; 
+    const imgH = img.height; 
+    const calcW = 300 * imgW / imgH;
+    const styles = {
+      backgroundImage: `url(${this.props.post.photoUrl})`,
+      flexGrow: 1,
+      flexShrink: 0,
+      flexBasis: `${calcW}px`,
+    };
+  
+    if (this.state) {
+      return (
+        <div className="post-grid-item" style={ styles }>
+          <Link to={`/posts/${this.props.post.id}`}></Link>
+          <div className="item-top item-info"></div>
+          <div className="item-bottom item-info"></div>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+}
 
 export default PostIndexItem;
