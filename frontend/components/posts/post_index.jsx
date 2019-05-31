0aab32922441f22c73fa2a9a4c32cd46785276ba
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-05-31 09:00:16 -0700

    Tweaked some styling to the post index page

--- frontend/components/posts/post_index.jsx
+++ frontend/components/posts/post_index.jsx
@@ -9,9 +9,15 @@ class PostIndex extends React.Component {
   render() {
     const postIndexItems = this.props.posts.map( post => <PostIndexItem key={post.id} post={post} />);
     return (
-      <section className="posts-grid">
-        {postIndexItems}
-      </section>
+      <div className="post-index">
+        <div className="post-index-header">
+          <h1>What's popular today</h1>
+          <p>See recently added photos</p>
+        </div>
+        <div className="posts-grid">
+          {postIndexItems}
+        </div>
+      </div>
     );
   }
 } 
