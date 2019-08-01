b2f0085d93b7e1a03e07115f8c83f8c90b905fee
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-07-31 18:35:51 -0700

    Removes Redux logger from production

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -4,7 +4,11 @@ import logger from "redux-logger";
 import RootReducer from "../reducers/root_reducer";
 
 const configureStore = (preloadedState = {}) => {
-  return createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger));
+  if (process.NODE_ENV === "production") {
+    return createStore(RootReducer, preloadedState, applyMiddleware(thunk));
+  } else {
+    return createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger));
+  }
 };
 
 export default configureStore;
