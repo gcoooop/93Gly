5efea90fdf946860fd3ad81e81be9920d29a368a
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-08-01 19:31:34 -0700

    Corrects environment variable path

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -4,7 +4,7 @@ import logger from "redux-logger";
 import RootReducer from "../reducers/root_reducer";
 
 const configureStore = (preloadedState = {}) => {
-  if (process.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === "production") {
     return createStore(RootReducer, preloadedState, applyMiddleware(thunk));
   } else {
     return createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger));
