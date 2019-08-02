0a9dec570dd3540f3028b32bca1f6a16f3410afa
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-08-02 16:51:26 -0700

    Adds actions to CRUD comments

---
+++ frontend/actions/comment_actions.js
@@ -0,0 +1,48 @@
+import * as CommentUtil from "../util/comment_util";
+
+export const RECEIVE_COMMENTS = "RECEIVE_COMMENTS";
+export const RECEIVE_COMMENT = "RECEIVE_COMMENT";
+export const CREATE_COMMENT = "CREATE_COMMENT";
+export const REMOVE_COMMENT = "REMOVE_COMMENT";
+
+export const receiveComments = comments => {
+  return {
+    type: RECEIVE_COMMENTS,
+    comments
+  };
+};
+
+export const receiveComment = comment => {
+  return {
+    type: RECEIVE_COMMENT,
+    comment
+  };
+};
+
+export const removeComment = comment => {
+  return {
+    type: REMOVE_COMMENT,
+    id: comment.id
+  };
+};
+
+export const fetchCommentsByPostId = postId => {
+  return dispatch => {
+    return CommentUtil.fetchComments(postId)
+      .then( comments => receiveComments(comments) );
+  };
+};
+
+export const createComment = comment => {
+  return dispatch => {
+    return CommentUtil.createComment(comment)
+      .then( comment => dispatch(receiveComment(comment)) );
+  };
+};
+
+export const deleteComment = comment => {
+  return dispatch => {
+    return Comment.deleteComment(comment)
+      .then( comment => dispatch(removeComment(comment)) );
+  };
+};
