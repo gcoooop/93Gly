2f5a46666ce46ea8ce6a9681602c9606bbb0e09b
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-08-07 17:11:37 -0700

    Refines the create comment components UI

--- frontend/components/comments/create/create_comment.jsx
+++ frontend/components/comments/create/create_comment.jsx
@@ -3,15 +3,34 @@ import React from "react";
 class CreateComment extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { body: "", postId: props.postId };
+    this.state = { body: "", showCommentControls: false };
 
+    this.toggleCommentControls = this.toggleCommentControls.bind(this);
+    this.calcTextareaHeight = this.calcTextareaHeight.bind(this);
     this.handleInput = this.handleInput.bind(this);
     this.cancelComment = this.cancelComment.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  toggleCommentControls(event) {
+    if (this.state.showCommentControls) {
+      if (!this.state.body){
+        this.setState({ showCommentControls: false });
+      }
+    } else {
+      this.setState({ showCommentControls: true });
+    }
+  }
+
+  calcTextareaHeight() {
+    const textareaEle = document.getElementById("comment-create-textarea");
+    textareaEle.style.height = "";
+    textareaEle.style.height = `${textareaEle.scrollHeight}px`;
+  }
+
   handleInput(event) {
     this.setState({ body: event.target.value });
+    this.calcTextareaHeight();
   }
 
   cancelComment(event) {
@@ -21,15 +40,16 @@ class CreateComment extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    this.props.createComment(this.state);
-    this.setState({ body: "" });
+    const comment = { body: this.state.body, postId: this.props.postId };
+    this.props.createComment(comment);
+    this.setState({ body: "", showCommentControls: false });
   }
 
   render() {
-    const commentControls = this.state.body ? (
+    const commentControls = this.state.showCommentControls ? (
       <div className="comment-create-controls">
         <button className="comment-create-cancel" onClick={this.cancelComment}>Cancel</button>
-        <button className="comment-create-submit" onClick={this.handleSubmit}>Comment</button>
+        <button className="comment-create-submit" disabled={!this.state.body} onClick={this.handleSubmit}>Comment</button>
       </div>
     ) : null;
 
@@ -39,7 +59,14 @@ class CreateComment extends React.Component {
           {/* avatar */}
         </div>
         <div className="comment-create-input">
-          <textarea placeholder="Add a comment" value={this.state.body} onChange={this.handleInput}/>
+          <textarea 
+            id="comment-create-textarea"
+            placeholder="Add a comment" 
+            value={this.state.body} 
+            onChange={this.handleInput} 
+            onFocus={this.toggleCommentControls}
+            onBlur={this.toggleCommentControls}
+          />
           <div className="comment-create-icon-container"><i className="far fa-comment"></i></div>
         </div>
         { commentControls }
